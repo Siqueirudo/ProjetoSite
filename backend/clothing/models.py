6d72d113b1e90e01a6c2ce2cb9c01317f38a8b2d
Siqueira <joaopedrosiqueira2467@gmail.com>
2024-12-05 03:28:43 -0300

    1.6v

--- backend/clothing/models.py
+++ backend/clothing/models.py
@@ -6,5 +6,5 @@ class cloth(models.Model):
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=200)
     size = models.CharField(max_length=4)
-    value = models.IntegerField(blank=True, null=True)
-    amount = models.FloatField(blank=True, null=True)
+    value = models.FloatField(blank=True, null=True)
+    amount = models.IntegerField(blank=True, null=True)
